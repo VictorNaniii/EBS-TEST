6f6f3dbb48a2bbcbd4e468c01c1975f0cffc5d0f
VictorNaniii <victor.nani@iis.utm.md>
2024-12-03 12:35:10 +0200

    Initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import { FaShoppingCart } from 'react-icons/fa'
+
+function Header() {
+  return (
+    <header>
+      <div>Company Name</div>
+      <FaShoppingCart />
+    </header>
+  )
+}
+
+export default Header
